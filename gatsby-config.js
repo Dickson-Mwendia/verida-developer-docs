ec3c819efabd8585c9adaabc4fb5ace68fe5f702
Dickson <dicksonmwendia99@gmail.com>
2021-07-27 20:34:53 +0300

    Added docz theme and documentation mdx files

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    "gatsby-theme-docz",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
